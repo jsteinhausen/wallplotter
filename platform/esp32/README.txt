17cf0b9ff76e1a1c8f15b2a5fdea88c798682535
jsteinhausen <johannes.steinhausen@gmail.com>
2022-12-07 23:29:44 +0100

    Analog input on pin 12 works+output on serial monitor

--- platform/esp32/README.txt
+++ platform/esp32/README.txt
@@ -1,2 +1,4 @@
 This is the base structure for the ESP32
+Use "pio run -e esp32dev -t upload" to Complile and deploy
+Use "pio device monitor" to activate the serial monitor
 
--- platform/esp32/src/main.cpp
+++ platform/esp32/src/main.cpp
@@ -1,28 +1,53 @@
-/*
- * Blink
- * Turns on an LED on for one second,
- * then off for one second, repeatedly.
- */
 
+/*
+  Analog input, analog output, serial output
+
+  Reads an analog input pin, maps the result to a range from 0 to 255 and uses
+  the result to set the pulse width modulation (PWM) of an output pin.
+  Also prints the results to the Serial Monitor.
+
+  The circuit:
+  - potentiometer connected to analog pin 0.
+    Center pin of the potentiometer goes to the analog pin.
+    side pins of the potentiometer go to +5V and ground
+  - LED connected from digital pin 9 to ground through 220 ohm resistor
+
+  created 29 Dec. 2008
+  modified 9 Apr 2012
+  by Tom Igoe
+
+  This example code is in the public domain.
+
+  https://www.arduino.cc/en/Tutorial/BuiltInExamples/AnalogInOutSerial
+*/
 #include "Arduino.h"
 
-// Set LED_BUILTIN if it is not defined by Arduino framework
-#define LED_BUILTIN 2
+// These constants won't change. They're used to give names to the pins used:
+const int analogInPin = 12;  // Analog input pin that the potentiometer is attached to
 
-void setup()
-{
-    // initialize LED digital pin as an output.
-    pinMode(LED_BUILTIN, OUTPUT);
+
+int sensorValue = 0;        // value read from the pot
+int outputValue = 0;        // value output to the PWM (analog out)
+
+void setup() {
+    // initialize serial communications at 9600 bps:
+    Serial.begin(9600);
 }
 
-void loop()
-{
-    // turn the LED on (HIGH is the voltage level)
-    digitalWrite(LED_BUILTIN, HIGH);
-    // wait for a second
-    delay(1000);
-    // turn the LED off by making the voltage LOW
-    digitalWrite(LED_BUILTIN, LOW);
-    // wait for a second
-    delay(1000);
+void loop() {
+    // read the analog in value:
+    sensorValue = analogRead(analogInPin);
+    // map it to the range of the analog out:
+    outputValue = map(sensorValue, 0, 4095, 0, 255);
+
+    // print the results to the Serial Monitor:
+    Serial.print("sensor = ");
+    Serial.print(sensorValue);
+    Serial.print("\t output = ");
+    Serial.println(outputValue);
+
+    // wait 2 milliseconds before the next loop for the analog-to-digital
+    // converter to settle after the last reading:
+    delay(200);
 }
+
