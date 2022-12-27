9de11862e5adcab72cc5829df503996a2bb12982
Msophys1 <matteo.sophys@students.fhnw.ch>
2022-12-27 18:39:01 +0100

    Frist try for this commit

--- platform/esp32/src/main.cpp
+++ platform/esp32/src/main.cpp
@@ -5,19 +5,15 @@
  */
 
 #include "Arduino.h"
-
 // Set LED_BUILTIN if it is not defined by Arduino framework
 #define LED_BUILTIN 2
+// Déclarez une variable pour suivre l'état précédent de l'interrupteur
+int previousSwitchState = LOW;
 
 void setup()
-{
-    // initialize LED digital pin as an output.
+{    // turn the LED on (HIGH is the voltage level)
+// initialize LED digital pin as an output.
     pinMode(LED_BUILTIN, OUTPUT);
-}
-
-void loop()
-{
-    // turn the LED on (HIGH is the voltage level)
     digitalWrite(LED_BUILTIN, HIGH);
     // wait for a second
     delay(1000);
@@ -25,4 +21,33 @@ void loop()
     digitalWrite(LED_BUILTIN, LOW);
     // wait for a second
     delay(1000);
+    // Initialisez le port en entrée
+   pinMode(2, INPUT);
+}
+void loop(){
+  // Lisez l'état de l'interrupteur
+  int currentSwitchState = digitalRead(2);
+
+  // Si l'état de l'interrupteur a changé depuis la dernière itération
+  if (currentSwitchState != previousSwitchState) {
+    // Si l'interrupteur est enfoncé (LOW), démarrez la fonction
+    if (currentSwitchState == LOW) {
+      startFunction();
+    }
+    // Si l'interrupteur est relâché (HIGH), arrêtez la fonction
+    else if (currentSwitchState == HIGH) {
+      stopFunction();
+    }
+  }
+
+  // Mettez à jour l'état précédent de l'interrupteur
+  previousSwitchState = currentSwitchState;
+}
+
+void startFunction() {
+  // Insérez ici le code à exécuter lorsque la fonction est démarrée
+}
+
+void stopFunction() {
+  // Insérez ici le code à exécuter lorsque la fonction est arrêtée
 }
