ba24c5cd4c802d5a4e2e151df0ed29a4e2e53384
jsteinhausen <johannes.steinhausen@gmail.com>
2023-01-02 13:59:48 +0100

    blinking led for uart added but not working

--- platform/esp32/src/main.cpp
+++ platform/esp32/src/main.cpp
@@ -9,6 +9,7 @@
 #include <SoftwareSerial.h>
 #define UART_ESP_TX 17
 #define UART_ESP_RX 16
+#define TEST_LED 2
 
 
 SoftwareSerial uart_esp(UART_ESP_RX, UART_ESP_TX);
@@ -17,13 +18,17 @@ SoftwareSerial uart_esp(UART_ESP_RX, UART_ESP_TX);
 void setup()
 {
     uart_esp.begin(9600);
+    pinMode(TEST_LED, OUTPUT);
+
 
 }
 void loop() {
 
     //Test Uart
     uart_esp.write("1");
+    digitalWrite(TEST_LED,HIGH);
     delay(5000000);
     uart_esp.write("0");
+    digitalWrite(TEST_LED,LOW);
 
 }
